92488e958feef0946e97cdd1d18787c18c7ef27d
Eugene <93196991+chernyaeugene@users.noreply.github.com>
2022-09-05 18:17:27 +0300

    adaptive finish

--- Js.js
+++ Js.js
@@ -10,3 +10,14 @@ const swiper = new Swiper('.swiper', {
     },
     speed: 2500,
 });
+
+// Бургер
+const iconMenu = document.querySelector('.menu_icon');
+const menuBody = document.querySelector('.header_menu');
+if (iconMenu){
+    iconMenu.addEventListener("click", function (e) {
+        document.body.classList.toggle('_lock')
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    })
+}
